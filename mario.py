573b53c6154f3f152017eb5ea57c09d6b86986b7
nankeyimeng <1980744819@qq.com>
2019-03-18 23:20:39 +0800

    third

---
+++ mario.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : mario.py
+# @Author: zixiao@ainirobot.com
+# @Date  : 2019-03-18
+# @Desc  :
+from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : run.py
+# @Author: zixiao@ainirobot.com
+# @Date  : 2019-03-18
+# @Desc  :
+from mario import env
+
+print(env.action_space)
+print(env.observation_space)
