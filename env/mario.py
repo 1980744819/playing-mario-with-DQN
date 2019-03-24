ac7c28fda496929893883399c133a975eb9cd34e
nankeyimeng <1980744819@qq.com>
2019-03-24 18:35:58 +0800

    add env

---
+++ env/mario.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : mario.py
+# @Author: zixiao@ainirobot.com
+# @Date  : 2019-03-21
+# @Desc  :
+import retro
+
+env = retro.make(game='SuperMarioBros-Nes')
